a808a9d5a82207bf72c8b5b5ff0f409f65535a4d
yanrong0225 <166887441+yanrong0225@users.noreply.github.com>
2024-04-13 16:22:58 +0800

    Add files via upload

---
+++ SC001_lecture02拷貝/BeeperRowAdv.py
@@ -0,0 +1,34 @@
+"""
+File: BeeperRowAdv.py
+Name:
+------------------------------
+This program makes Karel fill up
+Street 1 with some beepers already
+existed
+(This should be world insensitive)
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will fill the first Street in any world
+    """
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
---
+++ SC001_lecture02拷貝/Steeplechase.py
@@ -0,0 +1,21 @@
+"""
+File: Steeplechase.py
+Name: TODO:
+---------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel crosses hurdles in a 12x12 world
+    with a for loop 
+    """
+    pass
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
